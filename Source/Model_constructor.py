c0d4e3e49b66fac0f3adb6e588edbc2836d061bc
Jan Bińkowski <jan.binkowski96@gmail.com>
2019-07-10 16:00:21 +0200

    model selector method added, new class was created

--- Source/Model_constructor.py
+++ Source/Model_constructor.py
@@ -1,4 +1,5 @@
 from Ml_model_class import ML_model
+from sklearn.model_selection import GridSearchCV
 
 class Model_constructor(ML_model):
     def __init__(path, delimiter_type, model_type, best_model):
