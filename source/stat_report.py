36c4aedf39a75d7a690833dfbaac27eb3f2a9a97
Janbinkowski96 <jan.binkowski96@gmail.com>
2019-09-03 01:20:47 +0200

    added violin plot in case of  classification; plot and  stat data saving to file functions  updated

--- source/stat_report.py
+++ source/stat_report.py
@@ -3,6 +3,8 @@ import scipy.stats as stats
 import itertools
 import seaborn as sns 
 import pandas as pd
+import matplotlib.pyplot as plt
+import os
 
 class Report_generator():
 
@@ -16,37 +18,55 @@ class Report_generator():
     def prepare_data(self):
         self.data = pd.read_csv(self.data_set)
         self.col_names = list(self.data.columns)
-
-        for columns in self.col_names:
-            
-            try: #delete column if contain string
-                np.array(self.data[columns]).astype(float)
-            except:
-                self.col_names.remove(columns)
-                print("Column '{}' is not numerical so it can not be used in plot".format(columns))
+        
+        try:
+            os.mkdir("{}/{}".format(self.stat_report_path, self.project_name))
+        except FileExistsError:
+            pass
 
         #TODO add function for xls/xlsx files
 
     def desc_stat(self):
         """Method to get basic statistic"""
         desc_data = self.data.describe()
-        buf = "{}/Statisitical_report_{}.html".format(self.stat_report_path, self.project_name)
+        buf = "{}/{}/Statisitical_report_{}.html".format(self.stat_report_path, self.project_name, self.model_type)
         desc_data.to_html(buf)
 
-    
     def plot(self):
         """Method to generate relation-plot between all vs all in data"""
-        data_combinations = list(itertools.combinations(self.col_names, 2))
 
-        for var_x, var_y in data_combinations:
-            x_ax = self.data[var_x]
-            y_ax = self.data[var_y]
+        if self.model_type == "regression":
             
-            sns.set(style="darkgrid")
-            plot = sns.jointplot(x=x_ax, y=y_ax, kind='reg').annotate(stats.pearsonr)
-            plot.savefig("{}/{}_{}_vs_{}.png".format(self.stat_report_path, self.project_name, var_x, var_y))
+            data_combinations = list(itertools.combinations(self.col_names, 2))
 
+            for var_x, var_y in data_combinations:
+                x_ax = self.data[var_x]
+                y_ax = self.data[var_y]
+                
+                sns.set(style="darkgrid")
+                plot = sns.jointplot(x=x_ax, y=y_ax, kind='reg').annotate(stats.pearsonr)
+                plot.savefig("{}/{}/{}_vs_{}_{}.png".format(self.stat_report_path, self.project_name, var_x, var_y, self.model_type))
 
+        elif self.model_type == "classification":
+            #Generate violin-plot y_var vs all
 
+            quantitive_loc = self.col_names[-1] #y_var
+            self.col_names.remove(quantitive_loc)
 
+            for predictor in self.col_names:
+                plot = sns.violinplot(data=self.data, x=quantitive_loc, y=predictor)
+                fig = plot.get_figure()
+                fig.savefig("{}/{}/{}_vs_{}_{}.png".format(self.stat_report_path, self.project_name, quantitive_loc, predictor, self.model_type))
+                fig.clear()
+            #Generate plot all predictors vs all
+
+            data_combinations = list(itertools.combinations(self.col_names, 2))
+
+            for var_x, var_y in data_combinations:
+                x_ax = self.data[var_x]
+                y_ax = self.data[var_y]
+                
+                sns.set(style="darkgrid")
+                plot = sns.jointplot(x=x_ax, y=y_ax, kind='reg').annotate(stats.pearsonr)
+                plot.savefig("{}/{}/{}_vs_{}_{}.png".format(self.stat_report_path, self.project_name, var_x, var_y, self.model_type))
 
